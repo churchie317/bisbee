e5d2e6ec0c4549b266ca6b7249dfc33fd2cc4421
Eric Churchill <eric.churchill2@gmail.com>
2016-11-22 00:52:58 -0800

    [bug out] Correct props in index.js

--- bisbee-client/src/index.js
+++ bisbee-client/src/index.js
@@ -6,6 +6,6 @@ import './index.css';
 const pair = [ 'Trainspotting', '28 Days Later' ];
 
 ReactDOM.render(
-  <Voting pair={ pair } hasVoted="Trainspotting"/>,
+  <Voting pair={ pair } winner="Trainspotting" />,
   document.getElementById('root')
 );
