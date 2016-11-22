0e9b3addfc11488938607fab41ed4b7abfe6adeb
Eric Churchill <eric.churchill2@gmail.com>
2016-11-22 01:53:15 -0800

    [refactor] Refactor stateless components to functional components

--- bisbee-client/src/components/Vote.jsx
+++ bisbee-client/src/components/Vote.jsx
@@ -1,11 +1,30 @@
-import React from 'React';
-import Winner from './Winner';
-import Vote from './Vote';
+import React from 'react';
 
-export default voting = props => {
-  (<div>
-    { this.props.winner ?
-      <Winner ref="winner" winner={ this.props.winner } /> :
-      <Vote { ...this.props } /> }
+function getPair(pair) {
+  return pair || [];
+}
+
+function isDisabled(hasVoted) {
+  // RETURN false IF undefined
+  return !!hasVoted;
+}
+
+function hasVotedFor(hasVoted, entry) {
+  return hasVoted === entry;
+}
+
+export default (props) => {
+  return (<div className="voting">
+    { getPair(props.pair).map(entry => {
+      return (<button key={ entry }
+               disabled={ isDisabled(props.hasVoted) }
+               onClick={ () => props.vote(entry) }>
+        <h1>{ entry }</h1>
+        { hasVotedFor(props.hasVoted, entry) ?
+          <div className="label">Voted</div> :
+          null }
+      </button>)
+      }
+    )}
   </div>);
 }
--- bisbee-client/src/components/Winner.jsx
+++ bisbee-client/src/components/Winner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default Winner = props => {
-  (<div className="winner">
-    Winner is { this.props.winner }
+export default (props) => {
+  return (<div className="winner">
+    Winner is { props.winner }
   </div>);
 }
