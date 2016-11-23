3afd82bfb73927631e44fe282e54d1fa5560a830
Eric Churchill <eric.churchill2@gmail.com>
2016-11-23 01:48:13 -0800

    [feature] Add Redux store to app

--- bisbee-client/src/index.js
+++ bisbee-client/src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, hashHistory } from 'react-router';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
 import Routes from './index.jsx';
 
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: [ 'Trainspotting', '28 Days Later' ],
+      tally: {
+        'Sunshine': 2
+      }
+    }
+  }
+});
+
 ReactDOM.render(
-  <Router history={ hashHistory }>
-    { Routes }
-  </Router>,
+  <Provider store={ store }>
+    <Router history={ hashHistory }>
+      { Routes }
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
