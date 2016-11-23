7e225993fe286ea57ee20d453cfdd81c0384a7da
Eric Churchill <eric.churchill2@gmail.com>
2016-11-23 01:51:13 -0800

    [feature] Connect Voting stateless functional component to Redux store

--- bisbee-client/src/components/Voting.jsx
+++ bisbee-client/src/components/Voting.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import Winner from './Winner';
 import Vote from './Vote';
 
-export default (props) => {
+const Voting = (props) => {
   return (<div>
     { props.winner ?
       <Winner winner={ props.winner } /> :
       <Vote { ...props } /> }
   </div>);
 }
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn([ 'vote', 'pair' ]),
+    winner: state.get('winner')
+  };
+}
+
+const VotingContainer = connect(mapStateToProps)(Voting);
+
+export default VotingContainer;
