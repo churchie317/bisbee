a9eeb33e49c7b030e60c94cfc74015ecef77fe77
Eric Churchill <eric.churchill2@gmail.com>
2016-11-23 02:02:15 -0800

    [feature] Connect Results component to Redux store

--- bisbee-client/src/components/Results.jsx
+++ bisbee-client/src/components/Results.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Winner from './Winner';
 import Tally from './Tally';
 import Next from './Next';
 
-export default (props) => {
+const Results = (props) => {
   return props.winner ?
   <Winner winner={ props.winner} /> :
   <div className="results">
@@ -12,3 +13,13 @@ export default (props) => {
     <Next { ...props } />
   </div>;
 }
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn([ 'vote', 'pair' ]),
+    tally: state.getIn([ 'vote', 'tally' ]),
+    winner: state.get('winner')
+  }
+}
+
+export default connect(mapStateToProps)(Results);
