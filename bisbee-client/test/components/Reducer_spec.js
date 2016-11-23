1f05f19eff6e6306e5511c76460e6f555ea414a7
Eric Churchill <eric.churchill2@gmail.com>
2016-11-23 12:18:23 -0800

    [feature] Write reducer specs for VOTE action

--- bisbee-client/test/components/Reducer_spec.js
+++ bisbee-client/test/components/Reducer_spec.js
@@ -78,4 +78,48 @@ describe('reducer', () => {
     }));
   });
 
+  it('should handle VOTE by setting hasVoted to true', () => {
+    const state = fromJS({
+      vote: {
+        pair: [ 'Trainspotting', '28 Days Later' ],
+        tally: {
+          'Trainspotting': 1
+        }
+      }
+    });
+    const action = { type: VOTE, entry: 'Trainspotting'};
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: [ 'Trainspotting', '28 Days Later' ],
+        tally: {
+          'Trainspotting': 2
+        }
+      },
+      hasVoted: 'Trainspotting'
+    }));
+  });
+
+  it('should not set hasVoted for VOTE on invalid entry', () => {
+    const state = fromJS({
+      vote: {
+        pair: [ 'Trainspotting', '28 Days Later' ],
+        tally: {
+          'Trainspotting': 1
+        }
+      }
+    });
+    const action = { type: 'VOTE', entry: 'Sunshine' };
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: [ 'Trainspotting', '28 Days Later' ],
+        tally: {
+          'Trainspotting': 1
+        }
+      }
+    }));
+  })
 })
