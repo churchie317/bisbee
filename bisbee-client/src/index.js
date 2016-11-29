d5036afa5c3e0ace92ceff564efb80e58dce893b
Eric Churchill <eric.churchill2@gmail.com>
2016-11-28 16:41:33 -0800

    [feature] Incorporate middleware into Redux store creation

--- bisbee-client/src/index.js
+++ bisbee-client/src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, hashHistory } from 'react-router';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import { Provider } from 'react-redux';
 import Routes from './index.jsx';
+import { setState } from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: [ 'Trainspotting', '28 Days Later' ],
-      tally: {
-        'Sunshine': 2
-      }
-    }
-  }
+const socket = io(`${location.protocol}//${location.hostname}:7777`);
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+socket.on('state', state => {
+  store.dispatch(setState(state));
 });
 
 ReactDOM.render(
