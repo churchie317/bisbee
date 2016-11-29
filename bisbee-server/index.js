fdb1bff90a45dca61f3fc7ddbb3d58ff7451b39b
Eric Churchill <eric.churchill2@gmail.com>
2016-11-29 13:34:32 -0800

    [dev] Preload choices into Redux store

--- bisbee-server/index.js
+++ bisbee-server/index.js
@@ -2,4 +2,6 @@ import makeStore from './src/store';
 import startServer from './src/server';
 
 export const store = makeStore();
+store.dispatch({ type: 'SET_ENTRIES', entries: require('./entries.json') });
+console.log(store.getState());
 startServer(store);
