e2b85a33c8d53bddc29f229d69189bd1f368abcc
Eric Churchill <eric.churchill2@gmail.com>
2016-11-29 18:11:42 -0800

    [feature] Allow electron app to communicate with socket.io server

--- bisbee-client/src/index.js
+++ bisbee-client/src/index.js
@@ -9,7 +9,12 @@ import Routes from './index.jsx';
 import { setState } from './action_creators';
 import remoteActionMiddleware from './remote_action_middleware';
 
-const socket = io(`${location.protocol}//${location.hostname}:7777`);
+let socket;
+if( location.hostname && location.protocol ){
+  socket = io(`${location.protocol}//${location.hostname}:7777`);
+} else {
+  socket = io('http://localhost:7777');
+}
 const createStoreWithMiddleware = applyMiddleware(
   remoteActionMiddleware(socket)
 )(createStore);
